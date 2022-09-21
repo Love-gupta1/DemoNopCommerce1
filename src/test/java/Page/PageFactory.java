ab9dcb07548f2a761dd2adb4060e05a65afbc2e5
Love-gupta1 <love.gupta@infostride.com>
2022-09-21 18:09:11 +0530

    Updated

--- src/test/java/Page/PageFactory.java
+++ src/test/java/Page/PageFactory.java
@@ -3,7 +3,8 @@ import org.openqa.selenium.WebDriver;
 public class PageFactory {
     static WebDriver driver;
     private UserRegistration userRegisteration;
-
+    private Computer computer;
+    private Login login;
     public PageFactory(WebDriver driver) {
         this.driver = driver;
     }
@@ -13,5 +14,18 @@ public class PageFactory {
             userRegisteration = new UserRegistration(driver);
         }
         return userRegisteration;
+    }
+    public Computer getComputer() {
+        if(computer == null) {
+            computer = new Computer(driver);
+        }
+        return computer;
+    }
+
+    public Login getLogin() {
+        if (login == null) {
+            login = new Login(driver);
+        }
+        return  login;
     }
 }
